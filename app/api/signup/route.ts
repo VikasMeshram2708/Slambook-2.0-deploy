e45a2c0c52749c41ee9d65267e24b2d5f55b9b29
VikasMeshram2708 <meshramvikas744@gmail.com>
2024-02-13 14:00:39 +0530

    :bug: updated the signup api in terms of security purpose.

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,32 +1,17 @@
+/* eslint-disable no-console */
 /* eslint-disable import/prefer-default-export */
 import { PrismaClient } from '@prisma/client';
 import { NextRequest, NextResponse } from 'next/server';
-
 import bcrypt from 'bcryptjs';
-
 import * as z from 'zod';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 const prisma = new PrismaClient();
 
 const UserSchema = z.object({
-  name: z
-    .string()
-    .min(2, {
-      message: 'Minimum 2 characters are required',
-    })
-    .max(100, {
-      message: 'Maximum 100 characters are allowed.',
-    }),
+  name: z.string().min(2).max(100),
   email: z.string().email(),
-  password: z
-    .string()
-    .min(6, {
-      message: 'Minimum 2 characters are required',
-    })
-    .max(100, {
-      message: 'Maximum 100 characters are allowed.',
-    }),
+  password: z.string().min(6).max(100),
 });
 
 type UserType = z.infer<typeof UserSchema>;
@@ -36,61 +21,37 @@ export async function POST(req: NextRequest) {
     const userInput: UserType = UserSchema.parse(await req.json());
     const { name, email, password } = userInput;
 
-    // if email is already presnt throw error
     const userExists = await prisma.user.findUnique({
-      where: {
-        email,
-      },
+      where: { email },
     });
 
     if (userExists) {
       return NextResponse.json(
-        {
-          success: false,
-          message: 'Email already in use.',
-        },
-        {
-          status: 422,
-        },
+        { success: false, message: 'Email already in use.' },
+        { status: 422 },
       );
     }
 
+    // hashing the password
     const hashedPassword = await bcrypt.hash(password, 10);
     await prisma.user.create({
-      data: {
-        name,
-        email,
-        password: hashedPassword,
-      },
+      data: { name, email, password: hashedPassword },
     });
+
     return NextResponse.json(
-      {
-        success: true,
-        message: 'User registered successfully.',
-      },
-      {
-        status: 200,
-      },
+      { success: true, message: 'User registered successfully.' },
+      { status: 200 },
     );
-  } catch (e) {
-    if (e instanceof PrismaClientKnownRequestError) {
-      return NextResponse.json(
-        {
-          message: e.message,
-          stack: e.stack,
-        },
-        {
-          status: 500,
-        },
-      );
+  } catch (error) {
+    console.error('Error during user registration:', error);
+    let errorMessage = 'An unknown error occurred.';
+
+    if (error instanceof z.ZodError) {
+      errorMessage = error.errors[0].message;
+    } else if (error instanceof PrismaClientKnownRequestError) {
+      errorMessage = 'Database error. Please try again later.';
     }
-    return NextResponse.json(
-      {
-        message: e instanceof Error ? e.message : 'An unknown error occured.',
-      },
-      {
-        status: 500,
-      },
-    );
+
+    return NextResponse.json({ message: errorMessage }, { status: 500 });
   }
 }
