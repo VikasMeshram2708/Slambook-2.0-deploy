44f9ed68bb969d25cbe059588fe3abdfa99a4048
VikasMeshram2708 <meshramvikas744@gmail.com>
2024-02-13 14:15:28 +0530

    :fire: Created sign in api.

---
+++ app/api/signin/route.ts
@@ -0,0 +1,65 @@
+/* eslint-disable no-console */
+/* eslint-disable import/prefer-default-export */
+import { PrismaClient } from '@prisma/client';
+import { NextRequest, NextResponse } from 'next/server';
+import bcrypt from 'bcryptjs';
+import * as z from 'zod';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import connectToDb from '@/lib/ConnectToDb';
+
+const prisma = new PrismaClient();
+
+const UserSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6).max(100),
+});
+
+type UserType = z.infer<typeof UserSchema>;
+
+export async function POST(req: NextRequest) {
+  try {
+    const userInput: UserType = UserSchema.parse(await req.json());
+    const { email, password } = userInput;
+
+    // Connect to the database
+    await connectToDb();
+
+    // Find the user by email
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      return NextResponse.json(
+        { success: false, message: 'Email or password is invalid.' },
+        { status: 422 },
+      );
+    }
+
+    // Compare the password
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) {
+      return NextResponse.json(
+        { success: false, message: 'Email or password is invalid.' },
+        { status: 422 },
+      );
+    }
+
+    return NextResponse.json(
+      { success: true, message: 'User logged in successfully.' },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.error('Error during user signin:', error);
+    let errorMessage = 'An unknown error occurred.';
+
+    if (error instanceof z.ZodError) {
+      errorMessage = error.errors[0].message;
+    } else if (error instanceof PrismaClientKnownRequestError) {
+      errorMessage = 'Database error. Please try again later.';
+    }
+
+    return NextResponse.json({ message: errorMessage }, { status: 500 });
+  }
+}
